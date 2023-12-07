26679f418b172cec434298f3f811fac966a6f17d
andreyka101 <andrejkvasnin236@gmail.com>
2023-12-07 19:19:15 +0300

    first commit

---
+++ src/main.ts
@@ -0,0 +1,6 @@
+import './interfaces_GLASSlOLLIPOPS.scss'
+import LiveBackgroundMod from './live_background.ts'
+
+
+let liveBackground = new LiveBackgroundMod()
+liveBackground.startAnimationClassic()
